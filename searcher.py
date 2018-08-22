9ad1254451515d2be2608e5ac464b2c13891a5be
N e n e <makiasagawa@gmail.com>
2018-08-22 18:08:16 +0800

    Update searcher.py

--- searcher.py
+++ searcher.py
@@ -1,29 +1,51 @@
 import sys,os
+import json
+from ui import *
+from whoosh import qparser
 from whoosh.index import create_in,open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
-import json
-import whoosh.reading
 
-def searcher():
+
+def init():
 
 	sys.path.append("../")	
 	
 	data = _load_data("medical_records.json")
 
-	objects = _init()
-	ix = objects[0]
-	searcher = objects[1]
+	# create schema
+	global schema
+	schema = Schema(ID=ID(stored=True), NAME=TEXT(stored=True), BODY_TEXT=TEXT(stored=True))
+	if not os.path.exists("data_base"):
+		os.mkdir("data_base")
+	
+	# create index
+	global 
+	ix = create_in("data_base", schema) # for create new index
+	
+	# create searcher
+	global searcher
+	searcher = ix.searcher()
+	#parser = QueryParser("content", schema=ix.schema)
 
 	_write_data(ix,data)
 	
-	search_term = _get_search_term()
-	num = int(input("Enter the number of results: "))
+	search_term = ui.get_search_term()
 
-	query = QueryParser("content", schema=ix.schema).parse(search_term)
+	num = int(input("Number of results to show: "))
 
+
+def search( search_term ):
+	'''
+	and_search
+	or_search
+	fuzzy_search
+	'''
+
+	parser = QueryParser("content", schema = ix.schema).parse(search_term)
+	
 	with ix.searcher() as searcher:
-		results = searcher.search(query, limit = num)
+		results = searcher.search(parser, limit = num)
 		print(len(results))
 
 
@@ -36,67 +58,16 @@ def _load_data(file_name):
 	return data
 
 
-def _init():
-
-	# create schema
-	schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
-	if not os.path.exists("medical_record"):
-		os.mkdir("medical_record")
-	
-	# create index
-	ix = create_in("medical_record", schema) # for create new index
-	
-	# create searcher
-	searcher = ix.searcher()
-	#parser = QueryParser("content", schema=ix.schema)
-
-	return[ix,searcher]
-
-
 def _write_data(ix,data):
 
 	writer = ix.writer()
 	
 	for i in range(0,len(data)):
 	    writer.add_document(
-	        title=data[i]["id"],
-	        content=data[i]["body"]
+	        ID = data[i]["id"],
+	        NAME = data[i]["name"],
+	        BODY_TEXT = data[i]["body"]
 	    )
 
 	writer.commit()
 
-
-def _get_search_term():
-
-	search_term = input("Enter a keyword: ")
-
-	while True:
-	
-		q = input("Enter another keyword.(enter y to exit): ")
-		
-		if q == "y":
-			break
-		else:
-			search_term = search_term + " "+q
-
-	return search_term
-
-def _get_filter_term():
-
-	filter_terms = []
-	while True:
-		filter_term = []
-
-		quit = input("quit?(y/n)")	
-
-		if q == "q!":
-			break
-
-		else:
-			filter_term.append(input("Name of the filter term: "))
-			filter_term.append(input("Sign(>/</=): "))
-			filter_term.append(input("Numerical value: "))
-
-			filter_terms.append(filter_term)
-
-	return filter_terms
