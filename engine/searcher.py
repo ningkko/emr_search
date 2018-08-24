4ee83b028c4bf61320d84363aedf368c2dd71105
N e n e <makiasagawa@gmail.com>
2018-08-24 14:21:43 +0800

    Update searcher.py

--- engine/searcher.py
+++ engine/searcher.py
@@ -1,64 +1,56 @@
-import sys,os
 import json
-from ui import *
-from whoosh import qparser
+from whoosh import qparser,index
 from whoosh.index import create_in,open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
+import sys,os
+sys.path.append("../")  
+import config
 
 
 def init():
 
-	sys.path.append("../")	
+	dbPath = config.path["data_base"]
+	if not os.path.exists(dbPath):
+		os.mkdir(dbPath)
+		schema = create_schema()
+		ix = create_index(schema)
+		write_data(ix)
+
+
+def create_schema():
+
+	return Schema(ID=ID(stored=True), NAME=TEXT(stored=True), BODY_TEXT=TEXT(stored=True))
 	
-	data = _load_data("medical_records.json")
-
-	# create schema
-	global schema
-	schema = Schema(ID=ID(stored=True), NAME=TEXT(stored=True), BODY_TEXT=TEXT(stored=True))
-	if not os.path.exists("data_base"):
-		os.mkdir("data_base")
 	
-	# create index
-	global 
-	ix = create_in("data_base", schema) # for create new index
+def create_index(schema):
 	
-	# create searcher
-	global searcher
-	searcher = ix.searcher()
-	#parser = QueryParser("content", schema=ix.schema)
-
-	_write_data(ix,data)
+	dbPath = config.path["data_base"]
+	# create index	
+	if index.exists_in(dbPath):
+		return index.open_dir(dbPath)
+	else:
+		return create_in(dbPath,schema)
 	
-	search_term = ui.get_search_term()
 
-	num = int(input("Number of results to show: "))
-
-
-def search( search_term ):
+def search( ix, search_term ,num = 10):
 	'''
 	and_search
 	or_search
 	fuzzy_search
 	'''
 
-	parser = QueryParser("content", schema = ix.schema).parse(search_term)
+	parser = QueryParser("BODY_TEXT", schema = ix.schema).parse(search_term)
 	
 	with ix.searcher() as searcher:
 		results = searcher.search(parser, limit = num)
-		print(len(results))
+		print("%d results found.\r"%len(results))
+		for result in results:
+			print(result["ID"])
 
+def write_data(ix):
 
-def _load_data(file_name):
-
-	with open(file_name) as file:
-		for line in file:
-			data = json.loads(line)
-	
-	return data
-
-
-def _write_data(ix,data):
+	data = _load_data(config.config["data_path"]+"medical_records.json")
 
 	writer = ix.writer()
 	
@@ -66,8 +58,16 @@ def _write_data(ix,data):
 	    writer.add_document(
 	        ID = data[i]["id"],
 	        NAME = data[i]["name"],
-	        BODY_TEXT = data[i]["body"]
+	        BODY_TEXT = data[i]["text_body"]
 	    )
 
 	writer.commit()
 
+	
+def _load_data(file_name):
+
+	with open(file_name) as file:
+		for line in file:
+			data = json.loads(line)
+	
+	return data
