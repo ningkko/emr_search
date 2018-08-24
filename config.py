ef916c7f2d6041ccc240c5ffab55430f62da7f3e
N e n e <makiasagawa@gmail.com>
2018-08-24 14:18:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
+import os
+root_path = os.path.join(os.path.dirname(__file__))                                                                 
+config = {                                                                                                             
+        'data_path' : os.path.join(root_path, 'data/')                                                                  
+        }                                            
+
 path = {
-	"check_terms" : "../dicts/check.txt",
+	"check_terms" : "../dicts/jyxm.txt",
 	"userdict" : "../dicts/userdict.txt",
 	"medi" : "../dicts/medi.txt",
-	"stop_words" : "../dicts/stop.txt"
+	"stop_words" : "../dicts/stop.txt",
+	"data_base" : "../data_base"
 }
+
