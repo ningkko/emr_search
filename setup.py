5ca68c0c8815510e2a83e9dd5b79aec7fe4eadc2
N e n e <makiasagawa@gmail.com>
2018-08-27 15:47:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
 
-	name = "emr",
-	version = "1.0",
-	description = "A search tool for medical records.",
-	author = "Ningkko",
-	author_email = "makiasagawa@gmail.com",
-	packages = find_packages(),
-	install_requires = ["whoosh","jieba"],
-	url='https://github.com/ningkko/emr_search',
+    name="engine",
+    version="1.0",
+    description="A search tool for medical records.",
+    author="Ningkko",
+    author_email="makiasagawa@gmail.com",
+    packages=find_packages(),
+    install_requires=["whoosh", "jieba"],
+    url='https://github.com/ningkko/emr_search',
+    entry_points='''
+        [console_scripts]
+        emr=engine.emr
+    '''
 
-	)
+)
