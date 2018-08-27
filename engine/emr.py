c04a045a2d2981cd5a222c141fa05f892853fd72
N e n e <makiasagawa@gmail.com>
2018-08-27 15:44:29 +0800

    Update emr.py

--- engine/emr.py
+++ engine/emr.py
@@ -1,35 +1,30 @@
-from extract import clear_up,refresh
-from ui import *
-from searcher import *
-import sys,os
-sys.path.append("../")  
+from engine.extract import clear_up, refresh
+from engine.ui import *
+from engine.searcher import *
+import sys, os
+sys.path.append("../") 
 import config
-
-
-
 
 while True:
 
-	if not "medical_records.json" in os.listdir("../data"):
+    if not "medical_records.json" in os.listdir(config.config["data_path"]):
 
-		try:
-			print("BUILDING DATA...")
-			clear_up(config.config["data_path"]+"electronic-medical-record.json")
+        try:
+            print("BUILDING DATA...")
+            clear_up(config.config["data_path"]+"electronic-medical-record.json")
 
-		except IOError:
-			print("TARGET FILE NOT FOUND...")
-			break
+        except IOError:
+            print("TARGET FILE NOT FOUND...")
+            break
 
-	else:
+    else:
 
-		init()
-		ix = create_index(create_schema())
-		print("DATA BASE BUILT")
-		
-		while True:
-			
-			search_term = get_search_term()
-			#num = get_result_num()
-			search(ix,search_term)
-			print("----------SEARCHING DONE---------")
-		
+        init()
+        ix = create_index(create_schema())
+        print("DATA BASE BUILT")
+
+        while True:
+            search_term = get_search_term()
+            #num = get_result_num()
+            search(ix,search_term)
+            print("----------SEARCHING DONE---------")
